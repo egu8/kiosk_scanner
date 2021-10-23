cf9cc4d97f5a99ad7487493308636adf9a812e75
Eric Gu <egu8@gatech.edu>
2021-10-22 22:55:26 -0400

    Camera

---
+++ src/view/landing_page.js
@@ -0,0 +1,10 @@
+import AppStreamCam from '../util/camera';
+
+function LandingPage() {
+    return <div>
+        <h2>Video</h2>
+        <AppStreamCam/>
+    </div>;
+}
+
+export default LandingPage;
