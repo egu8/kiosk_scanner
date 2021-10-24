360f4ff3dd35e49e1a342cde284047fcbcbc17f6
Eric Gu <egu8@gatech.edu>
2021-10-24 01:02:07 -0400

    Working_bar_code

--- src/view_model/camera_logic.js
+++ src/view_model/camera_logic.js
@@ -1,6 +1,6 @@
 // Media Handler class
 import React from "react";
-import sendPic from "../model/camera_detection"
+import { sendPic, sendbarCode } from "../model/camera_detection"
 
 import cloneDeep from 'lodash/cloneDeep';
 
@@ -10,7 +10,7 @@ class AppStreamCam extends React.Component {
       this.streamCamVideo= this.streamCamVideo.bind(this);
 
       this.state = {
-        imgs: [],
+        bar_counter: 0
       }
 
       this.streamCamVideo()
@@ -20,7 +20,7 @@ class AppStreamCam extends React.Component {
       
       navigator.mediaDevices
         .getUserMedia(constraints)
-        .then(function(mediaStream) {
+        .then((mediaStream) => {
           var video = document.querySelector("video");
           
           var frames = []
@@ -28,12 +28,19 @@ class AppStreamCam extends React.Component {
             var video = document.querySelector("video");
             var canvas = document.querySelector("#videoCanvas");
             var ctx = canvas.getContext('2d');
+
+            // var canvas2 = document.createElement("canvas");
+            // var ctx2 = canvas.getContext('2d');
         
             canvas.width = video.videoWidth;
             canvas.height = video.videoHeight;
+
+            // canvas2.width = 300;
+            // canvas2.height = 300;
         
         
             ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+            // ctx2.drawImage(video, canvas.width/2 - faceArea/2, canvas.height/2 - faceArea/2, canvas2.width, canvas2.height);
 
             frames = [...frames, canvas.toDataURL()]
         
@@ -57,6 +64,7 @@ class AppStreamCam extends React.Component {
           video.ontimeupdate = function () {
               if (frames.length >= 16) {
                 sendPic(frames)
+                sendbarCode(frames)
                 frames = []
               }
           }
@@ -71,8 +79,8 @@ class AppStreamCam extends React.Component {
       return (
         <div>
           <div id="container">
-            <video style={{width: 1 }} muted ={true} autoPlay={true} id="videoElement"></video> 
-            <canvas id="videoCanvas"></canvas>
+            <video style={{width: 720 }} muted ={true} autoPlay={true} id="videoElement"></video> 
+            <canvas  style={{width: 1 }} id="videoCanvas"></canvas>
           </div>
           <br/>
         </div>
